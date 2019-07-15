c7628491d6c634a59ba78fd52819d9184c8756e5
Mason Ellwood <masonellwood@gmail.com>
2019-07-14 21:01:30 -0700

    created api call to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        // console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 
+                    'Client-ID 6eb40795698624385d4d26918223e77ffdf781fc2b259831b568edf70b7a7c54'
+            }    
+        });
     }
 
     render() {
